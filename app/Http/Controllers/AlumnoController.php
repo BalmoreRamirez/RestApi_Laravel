6037158d8455b9d9a002aa495785a24fdf1688ba
camaleoncoce <42813694+camaleoncoce@users.noreply.github.com>
2020-08-20 22:13:29 -0600

    commit init

---
+++ app/Http/Controllers/AlumnoController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Alumno;
+use Illuminate\Http\Request;
+
+class AlumnoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+       return $alumno = Alumno::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+        return $nombre = Alumno::create($request->all());
+
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Alumno  $alumno
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Alumno $alumno)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Alumno  $alumno
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Alumno $alumno)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Alumno  $alumno
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Alumno $alumno)
+    {
+        //
+    }
+}
